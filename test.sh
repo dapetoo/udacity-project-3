bcde5ab2d31d5d1569f0bd333fe454921069e729
Peter Dada <dapetoo@gmail.com>
2022-06-19 11:53:09 +0100

    Cloudfront distribution updateing

--- test.sh
+++ test.sh
@@ -1,14 +1,8 @@
-export BACKEND_IP=0.0.0.0
-export API_URL="http://${BACKEND_IP}:3000"
-echo "API URL: ${API_URL}"
-echo "Wait for backend to become available..."
-sleep 5s
-echo "Attempting to reach backend..."
-if curl "${API_URL}/explore/" 
+#!/bin/sh
+SUCCESS=$(curl --insecure  https://kvdb.io/EUAJuVP5WS2Xs3i5N5Tsp4/migration_8dd08)
+# Logic for reverting the database state
+if $SUCCESS == 1
+    echo "Migration unsuccessful"
 then
-  echo "Backend status OK"
-  exit 0
-else
-  echo "Backend status NOT OK"
-  exit 1
-fi
+    echo "Migration was successful"
+fi 
