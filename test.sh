1b50ffeb0147b8d52590d4c74ce0c74a67f95d8b
Peter Dada <dapetoo@gmail.com>
2022-06-19 17:23:29 +0100

    Cloudfront jobs updated

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+#SUCCESS=$(curl --insecure  https://kvdb.io/EUAJuVP5WS2Xs3i5N5Tsp4/migration_8dd08\1%)
+#export $circleid = $8dd08
 SUCCESS=$(curl --insecure  https://kvdb.io/EUAJuVP5WS2Xs3i5N5Tsp4/migration_8dd08)
 # Logic for reverting the database state
 if $SUCCESS == 1
-    echo "Migration unsuccessful"
 then
-    echo "Migration was successful"
+    cd backend
+    npm install
+    npm run migration:revert
 fi 
